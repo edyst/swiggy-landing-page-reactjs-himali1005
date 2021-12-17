1c254cba42dcd8b2b6dff56db81e2903d6960c88
himali1005 <70652072+himali1005@users.noreply.github.com>
2021-12-17 12:12:13 +0530

    Add files via upload

---
+++ swiggy-landing-page-reactjs-HIMALI/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Herosection from './Components/Herosection/hero'
+import Features from './Components/Features/features'
+import Restaurants from './Components/Restaurants/restaurants'
+import Footer from './Components/Footer/footer'
+
+import './App.css';
+import './Components/FontAwesome'
+
+
+function App() {
+  return (
+    <div className="body">
+      <Herosection />
+      <Features />
+      <Restaurants />
+      <Footer />
+      <footer> Himali 1910990450</footer>
+    </div>
+  )
+}
+
+export default App;
